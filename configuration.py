c2635e41115c915b676b4e7055c6520cb02aa3d2
Krendel <smirovaanna@yandex.ru>
2023-12-15 20:52:15 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://1c70bb31-aa40-4d6c-bf9a-b54bdf00966c.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
